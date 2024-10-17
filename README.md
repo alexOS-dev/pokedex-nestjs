f15b842742451c5a35e19187cf7c5d7cd1c53ef5
Alex Oliva <alexoliva87464@gmail.com>
2024-10-17 20:32:46 -0300

    init: Setup pokedex-nest project with mongo & mongoose

---
+++ README.md
@@ -0,0 +1,25 @@
+<p align="center">
+  <a href="http://nestjs.com/" target="blank"><img src="https://nestjs.com/img/logo-small.svg" width="120" alt="Nest Logo" /></a>
+</p>
+
+# Pokedex API
+
+1. Install dependencies
+
+```bash
+pnpm i
+```
+
+2. Run the docker-compose
+
+```bash
+docker-compose up -d
+```
+
+3. Run the application
+
+```bash
+pnpm start:dev
+```
+
+4. Go to `http://localhost:3000/api/v2/pokemons`
---
+++ src/app.module.ts
@@ -0,0 +1,18 @@
+import { Module } from '@nestjs/common';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { MongooseModule } from '@nestjs/mongoose';
+import { join } from 'path';
+import { PokemonModule } from './pokemon/pokemon.module';
+
+@Module({
+    imports: [
+        ServeStaticModule.forRoot({
+            rootPath: join(__dirname, '..', 'public'),
+        }),
+        PokemonModule,
+        MongooseModule.forRoot('mongodb://localhost:27017/nest-pokedex'),
+    ],
+    controllers: [],
+    providers: [],
+})
+export class AppModule {}
