ffdbdb4e48c5cf796bec6d0158fafa4ddfb6499e
Alex Oliva <alexoliva87464@gmail.com>
2024-10-19 11:45:34 -0300

    refactor: improve insertion seed

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -14,16 +14,22 @@ export class SeedService {
     private readonly axios: AxiosInstance = axios;
 
     async executeSeed() {
+        await this.pokemonModel.deleteMany({});
+
         const { data } = await this.axios.get<PokeResponse>(
             'https://pokeapi.co/api/v2/pokemon?limit=650',
         );
 
-        data.results.forEach(async ({ name, url }) => {
+        const pokemonToInsert: { name: string; no: number }[] = [];
+
+        data.results.forEach(({ name, url }) => {
             const segments = url.split('/');
             const no: number = +segments[segments.length - 2];
 
-            const pokemon = await this.pokemonModel.create({ name, no });
+            pokemonToInsert.push({ name, no });
         });
+
+        await this.pokemonModel.insertMany(pokemonToInsert);
 
         return 'Seed executed';
     }
